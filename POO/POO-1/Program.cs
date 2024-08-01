4abd5d9ed7797993936ad9bc45bc400bf6ac446f
lucas <lucasmartiniquinteros@gmail.com>
2024-07-31 22:59:20 -0300

    Primer intento c#

---
+++ POO/POO-1/Program.cs
@@ -0,0 +1,43 @@
+﻿public abstract class Empleado
+{
+    public string Nombre { get; set; }
+    public int Salario { get; set; }
+
+    public abstract int CalcularSalario();
+    public Empleado(string nombre, int salario)
+    {
+        Nombre = nombre;
+        Salario = salario;
+    }
+}
+
+public class EmpleadoTiempoCompleto: Empleado 
+{ 
+    public EmpleadoTiempoCompleto(string nombre, int salario) : base (nombre, salario) { }
+    public override int CalcularSalario()
+    {
+        return Salario;
+    }
+}
+
+public class EmpleadoMedioTiempo : Empleado
+{
+    public EmpleadoMedioTiempo(string nombre, int salario) : base(nombre, salario) { }
+
+    public override int CalcularSalario()
+    {
+        return Salario/2;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var empleadoTiempoCompleto = new EmpleadoTiempoCompleto("Pepito", 200);
+        Console.WriteLine($"El empleado {empleadoTiempoCompleto.Nombre} cobra: {empleadoTiempoCompleto.CalcularSalario()}");
+        var empleadoMedioTiempo = new EmpleadoMedioTiempo("Lukitas", 5000);
+        Console.WriteLine($"El empleado {empleadoTiempoCompleto.Nombre} cobra: {empleadoMedioTiempo.CalcularSalario()}");
+
+    }
+}
