3f6f2a984b3888117911c8a2066c9251b7fcda7b
Lucas <166059995+lucasmquinteros@users.noreply.github.com>
2024-08-07 17:42:24 -0300

    Add files via upload

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,25 +1,76 @@
-﻿float n1, n2;
-
-Console.Write("Ingrese un número: ");
-float.TryParse(Console.ReadLine(), out n1);
-
-
-Console.Write("Ingrese un número: ");
-float.TryParse(Console.ReadLine(), out n2);
-
-
-Console.WriteLine($"Suma: {n1 + n2}");
-
-Console.WriteLine($"Resta: {n1 - n2}");
-
-Console.WriteLine($"Multiplicación: {n1 * n2}");
-
-while(n2 == 0)
+﻿class Vehiculo
 {
-    Console.Write("Ingrese un valor distinto a 0\nAqui: ");
-    float.TryParse(Console.ReadLine(), out n2);
-}
-float Resultado = n1 / n2;
+    public string Marca { get; set; }
 
-Console.WriteLine($"Division: {Resultado}");
+    public string Modelo { get; set; }
+
+    public Vehiculo(string marca, string modelo)
+    {
+        Marca = marca;
+        Modelo = modelo;
+    }
+    protected void MostrarInfo()
+    {
+        Console.WriteLine($"\nMarca: {Marca} \nModelo: {Modelo}");
+    }
+}
+
+class Vehiculo4R : Vehiculo
+{
+    public int NumeroPuertas { get; set; }
+    
+    public Vehiculo4R(string marca, string modelo, int numeroPuertas) : base(marca, modelo)
+    {
+        NumeroPuertas = numeroPuertas;
+    }
+
+    protected void MostrarInfo4R()
+    {
+        MostrarInfo();
+        Console.WriteLine($"Numero de puertas: {NumeroPuertas}");
+    }
+}
+
+class Auto : Vehiculo4R
+{
+    public string TipoCombustible { get; set; }
+    
+    public Auto(string marca, string modelo, int numeroPuertas, string tipoCombustible) : base(marca, modelo, numeroPuertas)
+    {
+        TipoCombustible = tipoCombustible;
+    }
+    public void MostrarInfoAuto()
+    {
+        MostrarInfo4R();
+        Console.WriteLine($"Tipo de combustible: {TipoCombustible}");
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la marca: ");
+        string Marca = Console.ReadLine();
+
+        Console.Write("Ingrese el modelo: ");
+        string Modelo = Console.ReadLine();
+
+        Console.Write("Ingrese la cantidad de puertas: ");
+        int NumeroPuertas;
+        int.TryParse(Console.ReadLine(), out NumeroPuertas);
+
+        Console.Write("Ingrese tipo de combustible: ");
+        string TipoCombustible = Console.ReadLine();
+
+
+
+        Auto auto = new Auto(Marca, Modelo, NumeroPuertas, TipoCombustible);
+
+        auto.MostrarInfoAuto(); 
+    }
+}
+
+
 
